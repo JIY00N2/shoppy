1571729ef27af55c0fb24ab290b2ab51a617a6fe
JiYoon <angella990825@gmail.com>
2023-05-27 23:38:40 +0900

    로그인 사용자 아바타 구현

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom';
 import {FiShoppingBag} from 'react-icons/fi';
 import {BsFillPencilFill} from 'react-icons/bs';
 import { login, logout, onUserStateChange } from '../api/firebase';
+import User from './User';
 
 export default function Navbar() {
   const [user, setUser] = useState();
@@ -27,6 +28,7 @@ export default function Navbar() {
           <BsFillPencilFill>
           </BsFillPencilFill>
         </Link>
+        {user && <User user={user}></User>}
         {/* 사용자가 없다면 로그인 버튼을 보여줌*/}
         {!user && <button onClick ={login}>Login</button>}
         {/* 사용자가 있다면 로그아웃 버튼을 보여줌*/}
