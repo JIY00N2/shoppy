b3803ccc9d32a9c6b3db201876683a7fc969ba9a
JiYoon <angella990825@gmail.com>
2023-05-27 22:48:25 +0900

    tailwind 설정 및 헤더 스타일링

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,18 +5,18 @@ import {BsFillPencilFill} from 'react-icons/bs';
 
 export default function Navbar() {
   return (
-    <header>
+    <header className='flex justify-between border-b border-gray-300 p-2'>
       {/* 최상위 경로로 이동 */}
-      <Link to ='/'>
+      <Link to ='/' className='flex itmes-center text-4xl text-brand'>
         <FiShoppingBag>
           <h1>Shoppy</h1>
         </FiShoppingBag>
       </Link>
-      <nav>
+      <nav className='flex items-center gap-4 font-semibold'>
         {/* 상단에 다양한 nav 메뉴들  */}
         <Link to='/products'>Products</Link>
         <Link to='/carts'>Carts</Link>
-        <Link to='/products/new'>
+        <Link to='/products/new' className='text-2xl'>
           <BsFillPencilFill>
           </BsFillPencilFill>
         </Link>
